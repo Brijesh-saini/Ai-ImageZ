007e24e1e035c1b9b58f872cc3feccbddacda1b1
Brijesh-saini <sainibrijesh058@gmail.com>
2025-11-26 22:47:55 +0530

    Required changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,45 +1,229 @@
-// Copyright 2025 PREM
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     https://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// // Copyright 2025 PREM
+// // 
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// // 
+// //     https://www.apache.org/licenses/LICENSE-2.0
+// // 
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+
+// import userModel from "../models/userModel.js";
+// import bcrypt from 'bcryptjs'
+// import jwt from 'jsonwebtoken'
+// import validator from 'validator'
+// import razorpay from 'razorpay'
+// import transactionModel from "../models/transactionModel.js";
+
+// const registerUser = async (req, res) => {
+
+//     try {
+//         const { name, email, password } = req.body;
+
+//         // checking for all data to register user
+//         if (!name || !email || !password) {
+//             return res.json({ success: false, message: 'Missing Details' })
+//         }
+
+//         // validating email format
+//         if (!validator.isEmail(email)) {
+//             return res.json({ success: false, message: "Please enter a valid email" })
+//         }
+
+//         // validating strong password
+//         if (password.length < 8) {
+//             return res.json({ success: false, message: "Please enter a strong password" })
+//         }
+
+//         // hashing user password
+//         const salt = await bcrypt.genSalt(10); 
+//         const hashedPassword = await bcrypt.hash(password, salt)
+
+//         const userData = {
+//             name,
+//             email,
+//             password: hashedPassword,
+//         }
+
+//         const newUser = new userModel(userData)
+//         const user = await newUser.save()
+//         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+//         res.json({ success: true, token, user:{name: user.name} })
+
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
+
+// // API to login user
+// const loginUser = async (req, res) => {
+
+//     try {
+//         const { email, password } = req.body;
+//         const user = await userModel.findOne({ email })
+
+//         if (!user) {
+//             return res.json({ success: false, message: "User does not exist" })
+//         }
+
+//         const isMatch = await bcrypt.compare(password, user.password)
+
+//         if (isMatch) {
+//             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+//             res.json({ success: true, token })
+//         }
+//         else {
+//             res.json({ success: false, message: "Invalid credentials" })
+//         }
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
+
+// const userCredits = async (req, res) => {
+//     try {
+//         const {userId} = req.body
+
+//         const user = await userModel.findById(userId)
+//         res.json({success: true, credits: user.creditBalance, user:{name: user.name}})
+        
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
+
+// // const razorpayInstance = new razorpay ({
+// //     key_id: process.env.RAZORPAY_KEY_ID,
+// //     key_secret:process.env.RAZORPAY_KEY_SECRET
+// // }) ;
+
+// const paymentRazorpay = async (req, res) => {
+//     try {
+
+//         const { userId, planId } = req.body
+//         const userData = await userModel.findById(userId)
+
+//         if (!userData || !planId) {
+//             return res.json({ success: false, message: 'Missing Details' })
+//         }
+
+//         // creating options for razorpay payment
+//         let credits, plan, amount, date
+
+//         switch (planId) {
+//             case 'Basic':
+//                 plan = 'Basic'
+//                 credits = 25
+//                 amount = 10
+//                 break;
+//             case 'Advanced':
+//                 plan = 'Advanced'
+//                 credits = 70
+//                 amount = 30
+//                 break;
+//             case 'Premier':
+//                 plan = 'Premier'
+//                 credits = 150
+//                 amount = 50
+//                 break;
+        
+//             default:
+//                 return res.json({success: false, message: 'plan not found'});
+//         }
+
+//         date = Date.now();
+
+//         const transactionData ={
+//             userId, plan, amount, credits, date
+//         }
+
+//         const newTransaction = await transactionModel.create(transactionData)
+
+//         const options = {
+//             amount:amount * 100,
+//             currency: process.env.CURRENCY,
+//             receipt: newTransaction._id,
+//         }
+
+//         await razorpayInstance.orders.create(options, (error, order)=>{
+//             if (error) {
+//                 console.log(error);
+//                 return res.json({success:false, message:error})
+//             }
+//             res.json({success: true, order})
+//         })
+
+
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
+
+// // API to verify payment of razorpay
+// const verifyRazorpay = async (req, res) => {
+//     try {
+//         const { razorpay_order_id } = req.body
+//         const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+//         if (orderInfo.status === 'paid') {
+//             const transactionData = await transactionModel.findById(orderInfo.receipt)
+//             if (transactionData.payment) {
+//                 return res.json({success: false, message: 'Payment Failed'})
+//             }
+
+//             const userData = await userModel.findById(transactionData.userId)
+
+//             const creditBalance = userData.creditBalance + transactionData.credits
+//             await userModel.findByIdAndUpdate(userData._id, {creditBalance})
+//             await transactionModel.findByIdAndUpdate(transactionData._id, {payment:true})
+//             res.json({ success: true, message: "Credits Added" })
+//         }
+//         else {
+//             res.json({ success: false, message: 'Payment Failed' })
+//         }
+//     } catch (error) {
+//         console.log(error)
+//         res.json({ success: false, message: error.message })
+//     }
+// }
+
+// export {registerUser,loginUser, userCredits, paymentRazorpay, verifyRazorpay}
+
+
 
 import userModel from "../models/userModel.js";
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import validator from 'validator'
-import razorpay from 'razorpay'
-import transactionModel from "../models/transactionModel.js";
+// import razorpay from 'razorpay' // Removed
+// import transactionModel from "../models/transactionModel.js"; // Likely not needed if not tracking payments
 
 const registerUser = async (req, res) => {
-
+    // ... (Keep existing register logic) ...
+    // TIP: You can give more default credits here if you want
+    // const userData = { ..., creditBalance: 100 } 
     try {
         const { name, email, password } = req.body;
 
-        // checking for all data to register user
         if (!name || !email || !password) {
             return res.json({ success: false, message: 'Missing Details' })
         }
-
-        // validating email format
         if (!validator.isEmail(email)) {
             return res.json({ success: false, message: "Please enter a valid email" })
         }
-
-        // validating strong password
         if (password.length < 8) {
             return res.json({ success: false, message: "Please enter a strong password" })
         }
 
-        // hashing user password
         const salt = await bcrypt.genSalt(10); 
         const hashedPassword = await bcrypt.hash(password, salt)
 
@@ -47,6 +231,7 @@ const registerUser = async (req, res) => {
             name,
             email,
             password: hashedPassword,
+            creditBalance: 50 // Give 50 free credits on signup
         }
 
         const newUser = new userModel(userData)
@@ -61,10 +246,9 @@ const registerUser = async (req, res) => {
     }
 }
 
-// API to login user
 const loginUser = async (req, res) => {
-
-    try {
+   // ... (Keep existing login logic as is) ...
+   try {
         const { email, password } = req.body;
         const user = await userModel.findOne({ email })
 
@@ -88,6 +272,7 @@ const loginUser = async (req, res) => {
 }
 
 const userCredits = async (req, res) => {
+    // ... (Keep existing credits logic as is) ...
     try {
         const {userId} = req.body
 
@@ -100,100 +285,28 @@ const userCredits = async (req, res) => {
     }
 }
 
-// const razorpayInstance = new razorpay ({
-//     key_id: process.env.RAZORPAY_KEY_ID,
-//     key_secret:process.env.RAZORPAY_KEY_SECRET
-// }) ;
-
-const paymentRazorpay = async (req, res) => {
+// NEW FUNCTION: Add free credits without payment
+const addFreeCredits = async (req, res) => {
     try {
+        const { userId } = req.body;
+        const user = await userModel.findById(userId);
 
-        const { userId, planId } = req.body
-        const userData = await userModel.findById(userId)
-
-        if (!userData || !planId) {
-            return res.json({ success: false, message: 'Missing Details' })
+        if (!user) {
+            return res.json({ success: false, message: "User not found" });
         }
 
-        // creating options for razorpay payment
-        let credits, plan, amount, date
-
-        switch (planId) {
-            case 'Basic':
-                plan = 'Basic'
-                credits = 25
-                amount = 10
-                break;
-            case 'Advanced':
-                plan = 'Advanced'
-                credits = 70
-                amount = 30
-                break;
-            case 'Premier':
-                plan = 'Premier'
-                credits = 150
-                amount = 50
-                break;
+        // Add 10 credits (or any amount you prefer)
+        const updatedBalance = user.creditBalance + 10;
         
-            default:
-                return res.json({success: false, message: 'plan not found'});
-        }
+        await userModel.findByIdAndUpdate(user._id, { creditBalance: updatedBalance });
 
-        date = Date.now();
-
-        const transactionData ={
-            userId, plan, amount, credits, date
-        }
-
-        const newTransaction = await transactionModel.create(transactionData)
-
-        const options = {
-            amount:amount * 100,
-            currency: process.env.CURRENCY,
-            receipt: newTransaction._id,
-        }
-
-        await razorpayInstance.orders.create(options, (error, order)=>{
-            if (error) {
-                console.log(error);
-                return res.json({success:false, message:error})
-            }
-            res.json({success: true, order})
-        })
-
+        res.json({ success: true, message: "Free credits added", credits: updatedBalance });
 
     } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
+        console.log(error);
+        res.json({ success: false, message: error.message });
     }
 }
 
-// API to verify payment of razorpay
-const verifyRazorpay = async (req, res) => {
-    try {
-        const { razorpay_order_id } = req.body
-        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
-
-        if (orderInfo.status === 'paid') {
-            const transactionData = await transactionModel.findById(orderInfo.receipt)
-            if (transactionData.payment) {
-                return res.json({success: false, message: 'Payment Failed'})
-            }
-
-            const userData = await userModel.findById(transactionData.userId)
-
-            const creditBalance = userData.creditBalance + transactionData.credits
-            await userModel.findByIdAndUpdate(userData._id, {creditBalance})
-            await transactionModel.findByIdAndUpdate(transactionData._id, {payment:true})
-            res.json({ success: true, message: "Credits Added" })
-        }
-        else {
-            res.json({ success: false, message: 'Payment Failed' })
-        }
-    } catch (error) {
-        console.log(error)
-        res.json({ success: false, message: error.message })
-    }
-}
-
-export {registerUser,loginUser, userCredits, paymentRazorpay, verifyRazorpay}
+// Export the new function instead of payment ones
+export { registerUser, loginUser, userCredits, addFreeCredits }
--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ const app= express()
 
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+// Allow requests from your frontend
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 await connectDB()
 
 
